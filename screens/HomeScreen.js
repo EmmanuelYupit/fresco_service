9e2d728ae438e78954848954f49fb127a09b857f
EmmanuelYupit <emmanuelyupit08@gmail.com>
2020-07-03 11:36:42 -0500

    usgin only use effect, product detail now loading

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,223 +1,219 @@
-import * as WebBrowser from "expo-web-browser";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import * as WebBrowser from 'expo-web-browser';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  FlatList,
-  Dimensions,
-  Button,
-  RefreshControl,
-} from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import productImage from "../components/product/Image.js";
-import { MonoText } from "../components/StyledText";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-const width = Dimensions.get("window").width;
+    Image,
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    FlatList,
+    Dimensions,
+    Button,
+    RefreshControl,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import productImage from '../components/product/Image.js';
+import { MonoText } from '../components/StyledText';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const width = Dimensions.get('window').width;
 
 export default function HomeScreen({ navigation }) {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      // The screen is focused
-      // Call any action
-      setRefreshing(true);
-      const result = axios
-        .get("http://192.168.0.4:3000/api/v1/product")
-        .then((res) => {
-          setProducts(res.data);
-          setRefreshing(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setRefreshing(false);
-        });
-    });
-  }, []);
-  const [refreshing, setRefreshing] = React.useState(false);
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-
-    const result = axios
-      .get("http://192.168.0.4:3000/api/v1/product")
-      .then((res) => {
-        /*res.data.map((product) => {
-          console.log(product.imageUrl);
-          product.imageUrl = require(product.imageUrl);
-        });*/
-        setProducts(res.data);
+    useEffect(async () => {
+        setRefreshing(true);
+        const { data } = await axios.get(
+            'https://fresco.herokuapp.com/api/v1/product'
+        );
+        setProducts(data);
         setRefreshing(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setRefreshing(false);
-      });
-  }, [refreshing]);
+    }, []);
+    const [refreshing, setRefreshing] = React.useState(false);
 
-  function _renderItemFood(item) {
-    console.log(item);
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+
+        const result = axios
+            .get('https://fresco.herokuapp.com/api/v1/product')
+            .then((res) => {
+                setProducts(res.data);
+                setRefreshing(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setRefreshing(false);
+            });
+    }, [refreshing]);
+
+    function _renderItemFood(item) {
+        console.log(item);
+        return (
+            <TouchableOpacity
+                style={styles.divFood}
+                onPress={() => navigation.push('Detail')}
+            >
+                <Image
+                    style={styles.imageFood}
+                    resizeMode="contain"
+                    source={productImage(item.imageUrl)}
+                />
+                <View
+                    style={{
+                        height: width / 2 - 20 - 90,
+                        backgroundColor: 'transparent',
+                        width: width / 1 - 1 - 1,
+                    }}
+                />
+                <Text
+                    style={{
+                        fontWeight: 'bold',
+                        fontSize: 18,
+                        textAlign: 'center',
+                    }}
+                >
+                    {item.name}
+                </Text>
+                <Text style={{ fontSize: 15, textAlign: 'center' }}>
+                    {item.description.substring(0, 18)}...
+                </Text>
+                <Text style={{ fontSize: 20, color: 'green' }}>
+                    ${item.prices[0].price}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+
     return (
-      <TouchableOpacity
-        style={styles.divFood}
-        onPress={() => navigation.push("Detail")}
-      >
-        <Image
-          style={styles.imageFood}
-          resizeMode="contain"
-          source={productImage(item.imageUrl)}
-        />
-        <View
-          style={{
-            height: width / 2 - 20 - 90,
-            backgroundColor: "transparent",
-            width: width / 1 - 1 - 1,
-          }}
-        />
-        <Text style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}>
-          {item.name}
-        </Text>
-        <Text style={{ fontSize: 15, textAlign: "center" }}>
-          {item.description.substring(0, 18)}...
-        </Text>
-        <Text style={{ fontSize: 20, color: "green" }}>
-          ${item.prices[0].price}
-        </Text>
-      </TouchableOpacity>
+        <View style={styles.container}>
+            <ScrollView
+                style={styles.welcomeContainer}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+            >
+                <FlatList
+                    //horizontal={true}
+                    data={products}
+                    numColumns={3}
+                    renderItem={({ item }) => _renderItemFood(item)}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </ScrollView>
+
+            <View style={styles.tabBarInfoContainer}></View>
+        </View>
     );
-  }
-
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.welcomeContainer}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      >
-        <FlatList
-          //horizontal={true}
-          data={products}
-          numColumns={3}
-          renderItem={({ item }) => _renderItemFood(item)}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}></View>
-    </View>
-  );
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+    header: null,
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E9F0C1",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#E9F0C1',
+    },
 
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)",
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center",
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {},
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-  },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+        marginVertical: 7,
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: { width: 0, height: -3 },
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {},
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+    },
 
-  navigationFilename: {},
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center",
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7",
-  },
-  imageThumbnail: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 100,
-  },
-  imageBanner: {
-    height: width / 2,
-    width: width - 40,
-    borderRadius: 10,
-    marginHorizontal: 20,
-  },
-  divCategorie: {
-    backgroundColor: "red",
-    margin: 5,
-    alignItems: "center",
-    borderRadius: 10,
-    padding: 10,
-  },
-  titleCatg: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  imageFood: {
-    width: width / 3 - 30 - 10,
-    height: width / 2 - 20 - 30,
-    backgroundColor: "transparent",
-    position: "absolute",
-    top: -20,
-  },
-  divFood: {
-    width: width / 3 - 13,
-    padding: 10,
-    borderRadius: 10,
-    marginTop: 10,
-    marginBottom: 5,
-    marginLeft: 10,
-    alignItems: "center",
-    elevation: 8,
-    shadowOpacity: 0.3,
-    shadowRadius: 50,
-    backgroundColor: "#E9F0C1",
-  },
+    navigationFilename: {},
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+    },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+    imageThumbnail: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100,
+    },
+    imageBanner: {
+        height: width / 2,
+        width: width - 40,
+        borderRadius: 10,
+        marginHorizontal: 20,
+    },
+    divCategorie: {
+        backgroundColor: 'red',
+        margin: 5,
+        alignItems: 'center',
+        borderRadius: 10,
+        padding: 10,
+    },
+    titleCatg: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    imageFood: {
+        width: width / 3 - 30 - 10,
+        height: width / 2 - 20 - 30,
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: -20,
+    },
+    divFood: {
+        width: width / 3 - 13,
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 10,
+        marginBottom: 5,
+        marginLeft: 10,
+        alignItems: 'center',
+        elevation: 8,
+        shadowOpacity: 0.3,
+        shadowRadius: 50,
+        backgroundColor: '#E9F0C1',
+    },
 });
--- screens/ProductDetail.js
+++ screens/ProductDetail.js
@@ -1,210 +1,224 @@
-import { Ionicons } from "@expo/vector-icons";
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
-import axios from "axios";
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import axios from 'axios';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
 
-import { RectButton, ScrollView } from "react-native-gesture-handler";
-import Icon from "react-native-vector-icons/Ionicons";
-import productImage from "../components/product/Image";
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+import productImage from '../components/product/Image';
 
-const { height, width } = Dimensions.get("window");
+const { height, width } = Dimensions.get('window');
 export default function ProductDetail({ navigation }) {
-  const [product, setProduct] = React.useState({
-    price: "Loading..",
-    description: "Loading..",
-    product: {
-      imageUrl: require("../assets/images/products/aguacate.png"),
-    },
-  });
+    const [detail, setDetail] = React.useState({});
 
-  React.useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      const result = axios
-        .get("http://192.168.0.4:3000/api/v1/product/1")
-        .then((res) => {
-          //const imageUrl = `../assets/images/products/${res.data.product.imageUrl}.png`;
-          //res.data.product.imageUrl = require({ uri: imageUrl });
-          //setRefreshing(false);
-          res.data.product.imageUrl = require("../assets/images/products/aguacate.png");
-          console.log("aaa");
-          console.log(res.data.product.imageUrl);
-          setProduct(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-          alert("sssss");
-          setRefreshing(false);
-        });
-    });
-  });
+    React.useEffect(async () => {
+        let { data } = await axios.get(
+            'https://fresco.herokuapp.com/api/v1/product/1'
+        );
+        // data.product.imageUrl = require('../assets/images/products/aguacate.png');
+        setDetail(data);
+        console.log('====================================');
+        console.log(detail);
+        console.log('====================================');
+        // const unsubscribe = navigation.addListener('focus', () => {
+        //     const result = axios
+        //         .get('https://fresco.herokuapp.com/api/v1/product/1')
+        //         .then((res) => {
+        //             res.data.detail.imageUrl = require('../assets/images/products/aguacate.png');
+        //             console.log(res.data.detail.imageUrl);
+        //             setProduct(res.data);
+        //         })
+        //         .catch((err) => {
+        //             console.log(err);
+        //             alert('sssss');
+        //             setRefreshing(false);
+        //         });
+        // });
+    }, []);
 
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <View style={{ height: 20 }} />
-      <View style={{ height: 10 }} />
-
-      <View style={{ flex: 1 }}>
+    return (
         <View
-          style={{
-            width: width - 20,
-            margin: 10,
-            backgroundColor: "transparent",
-            flexDirection: "row",
-            borderColor: "#cccccc",
-          }}
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
         >
-          <Image
-            resizeMode={"contain"}
-            style={{ width: width / 2, height: width / 3 }}
-            source={productImage(item.imageUrl)}
-          />
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: "transparent",
-              padding: 10,
-              justifyContent: "space-between",
-            }}
-          >
-            <View>
-              <Text style={{ fontWeight: "bold", fontSize: 40 }}>
-                ${product.price}
-              </Text>
-              <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-                {product.unitType}
-              </Text>
+            <View style={{ height: 20 }} />
+            <View style={{ height: 10 }} />
+
+            <View style={{ flex: 1 }}>
+                <View
+                    style={{
+                        width: width - 20,
+                        margin: 10,
+                        backgroundColor: 'transparent',
+                        flexDirection: 'row',
+                        borderColor: '#cccccc',
+                    }}
+                >
+                    <Image
+                        resizeMode={'contain'}
+                        style={{ width: width / 2, height: width / 3 }}
+                        source={productImage('jitomate')}
+                    />
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'transparent',
+                            padding: 10,
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <View>
+                            <Text style={{ fontWeight: 'bold', fontSize: 40 }}>
+                                ${detail.price}
+                            </Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
+                                {detail.unitType}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        width: width - 20,
+                        margin: 10,
+                        backgroundColor: 'transparent',
+                        flexDirection: 'row',
+                        paddingBottom: 10,
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontWeight: 'bold',
+                            fontSize: 30,
+                            textAlign: 'center',
+                        }}
+                    >
+                        {/* ${detail.product.description} */}
+                    </Text>
+                </View>
+                <View
+                    style={{
+                        alignItems: 'center',
+                    }}
+                >
+                    <View
+                        style={{ flexDirection: 'row', alignItems: 'center' }}
+                    >
+                        <TouchableOpacity>
+                            <Icon
+                                name="ios-remove-circle"
+                                size={40}
+                                color={'#9fd236'}
+                            />
+                        </TouchableOpacity>
+                        <Text
+                            style={{
+                                paddingHorizontal: 8,
+                                fontWeight: 'bold',
+                                fontSize: 30,
+                            }}
+                        >
+                            5 kilos
+                        </Text>
+                        <TouchableOpacity>
+                            <Icon
+                                name="ios-add-circle"
+                                size={40}
+                                color={'#9fd236'}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={{ alignItems: 'center' }}>
+                    <Text
+                        style={{
+                            fontWeight: 'bold',
+                            color: '#9fd236',
+                            fontSize: 40,
+                            alignItems: 'center',
+                        }}
+                    >
+                        $565 Total
+                    </Text>
+                </View>
             </View>
-          </View>
-        </View>
-        <View
-          style={{
-            width: width - 20,
-            margin: 10,
-            backgroundColor: "transparent",
-            flexDirection: "row",
-            paddingBottom: 10,
-          }}
-        >
-          <Text
-            style={{ fontWeight: "bold", fontSize: 30, textAlign: "center" }}
-          >
-            ${product.product.description}
-          </Text>
-        </View>
-        <View
-          style={{
-            alignItems: "center",
-          }}
-        >
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <TouchableOpacity>
-              <Icon name="ios-remove-circle" size={40} color={"#9fd236"} />
-            </TouchableOpacity>
-            <Text
-              style={{
-                paddingHorizontal: 8,
-                fontWeight: "bold",
-                fontSize: 30,
-              }}
+
+            <TouchableOpacity
+                style={{
+                    backgroundColor: '#9fd236',
+                    width: width - 40,
+                    alignItems: 'center',
+                    padding: 10,
+                    borderRadius: 5,
+                }}
             >
-              5 kilos
-            </Text>
-            <TouchableOpacity>
-              <Icon name="ios-add-circle" size={40} color={"#9fd236"} />
+                <Text
+                    style={{
+                        fontSize: 24,
+                        fontWeight: 'bold',
+                        color: 'white',
+                    }}
+                >
+                    Agregar al carrito
+                </Text>
             </TouchableOpacity>
-          </View>
+
+            <View style={{ height: 20 }} />
         </View>
-
-        <View style={{ alignItems: "center" }}>
-          <Text
-            style={{
-              fontWeight: "bold",
-              color: "#9fd236",
-              fontSize: 40,
-              alignItems: "center",
-            }}
-          >
-            $565 Total
-          </Text>
-        </View>
-      </View>
-
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#9fd236",
-          width: width - 40,
-          alignItems: "center",
-          padding: 10,
-          borderRadius: 5,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 24,
-            fontWeight: "bold",
-            color: "white",
-          }}
-        >
-          Agregar al carrito
-        </Text>
-      </TouchableOpacity>
-
-      <View style={{ height: 20 }} />
-    </View>
-  );
+    );
 }
 
 function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton
-      style={[styles.option, isLastOption && styles.lastOption]}
-      onPress={onPress}
-    >
-      <View style={{ flexDirection: "row" }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
+    return (
+        <RectButton
+            style={[styles.option, isLastOption && styles.lastOption]}
+            onPress={onPress}
+        >
+            <View style={{ flexDirection: 'row' }}>
+                <View style={styles.optionIconContainer}>
+                    <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+                </View>
+                <View style={styles.optionTextContainer}>
+                    <Text style={styles.optionText}>{label}</Text>
+                </View>
+            </View>
+        </RectButton>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fafafa",
-  },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: "#fdfdfd",
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: "#ededed",
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: "flex-start",
-    marginTop: 1,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa',
+    },
+    contentContainer: {
+        paddingTop: 15,
+    },
+    optionIconContainer: {
+        marginRight: 12,
+    },
+    option: {
+        backgroundColor: '#fdfdfd',
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderBottomWidth: 0,
+        borderColor: '#ededed',
+    },
+    lastOption: {
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    optionText: {
+        fontSize: 15,
+        alignSelf: 'flex-start',
+        marginTop: 1,
+    },
 });
