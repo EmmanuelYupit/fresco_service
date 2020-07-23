ca55613a3e1fdc7fd30c5e4870222c13d697d2dc
EmmanuelYupit <emmanuelyupit08@gmail.com>
2020-07-23 00:19:58 -0500

    social media icons linking working

--- screens/OrderStateScreen.js
+++ screens/OrderStateScreen.js
@@ -1,4 +1,3 @@
-import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import global from '../store/global';
@@ -12,6 +11,8 @@ import {
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import { Card, Paragraph, Title, Button } from 'react-native-paper';
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
+import { Ionicons } from '@expo/vector-icons';
+import * as Linking from 'expo-linking';
 
 const { height, width } = Dimensions.get('window');
 
@@ -27,6 +28,37 @@ export default function OrderStateScreen() {
         { id: 3, name: 'recieved', label: 'Entregado' },
     ];
 
+    const socialMedia = [
+        {
+            id: 0,
+            name: 'logo-whatsapp',
+            onPress: () => {
+                Linking.openURL('https://wa.me/5219981546160');
+            },
+        },
+        {
+            id: 1,
+            name: 'logo-facebook',
+            onPress: () => {
+                Linking.openURL('https://facebook.com/centralde.abastos.50');
+            },
+        },
+        {
+            id: 2,
+            name: 'md-call',
+            onPress: () => {
+                Linking.openURL('tel:+123456789');
+            },
+        },
+        {
+            id: 3,
+            name: 'md-mail',
+            onPress: () => {
+                Linking.openURL('mailto: e.pyupit@gmail.com');
+            },
+        },
+    ];
+
     return (
         <View style={{ flex: 1, marginTop: 15 }}>
             <ProgressSteps completedLabelColor="#82749a">
@@ -38,6 +70,80 @@ export default function OrderStateScreen() {
                     ></ProgressStep>
                 ))}
             </ProgressSteps>
+            <View
+                style={{
+                    flex: 1,
+                    flexDirection: 'column-reverse',
+                    alignItems: 'center',
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        marginBottom: 20,
+                        marginTop: 20,
+                    }}
+                >
+                    {socialMedia.map(({ id, name, onPress }) => (
+                        <TouchableOpacity
+                            key={id}
+                            style={{
+                                flex: 3,
+                                alignItems: 'center',
+                            }}
+                            onPress={onPress}
+                        >
+                            <Ionicons
+                                style={{
+                                    color: '#82749a',
+                                }}
+                                name={name}
+                                size={30}
+                            />
+                        </TouchableOpacity>
+                    ))}
+                </View>
+                <View
+                    style={{
+                        fontSize: 24,
+                        width: width - 40,
+                        borderRadius: 5,
+                        backgroundColor: '#f8f9fa',
+                        borderColor: '#82749a',
+                        borderWidth: 2,
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            textAlign: 'center',
+                        }}
+                    >
+                        Incididunt sunt magna est tempor qui consequat. Est
+                        labore non ea non. Enim duis amet dolore id. Incididunt
+                        sunt magna est tempor qui consequat. Est labore non ea
+                        non. Enim duis amet dolore id.
+                    </Text>
+                </View>
+                <View
+                    style={{
+                        fontSize: 24,
+                        width: width - 40,
+                        borderRadius: 5,
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            textAlign: 'center',
+                        }}
+                    >
+                        Mensaje del proveedor:
+                    </Text>
+                </View>
+            </View>
+
             <View style={{ alignItems: 'center' }}>
                 <TouchableOpacity
                     disabled={global.order.current.id ? false : true}
@@ -47,7 +153,7 @@ export default function OrderStateScreen() {
                     //     })
                     // }
                     style={{
-                        backgroundColor: '#cccccc',
+                        backgroundColor: '#ff7f7f',
                         width: width - 40,
                         alignItems: 'center',
                         padding: 10,
